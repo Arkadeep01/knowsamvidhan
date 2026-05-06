7e489cb02afdf2642c692b7088e5116ef468fb02
Arkadeep01 <arjundevrath001@gmail.com>
2026-05-06 22:24:14 +0530

    Cursor Designed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { AudioProvider } from "@/context/AudioContext";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import CustomCursor from "@/components/cursor";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <CustomCursor />
         <AudioProvider>{children}</AudioProvider>
       </body>
     </html>
